59e3920d7296b55a3d26a281f9d07a0be24ed7bc
lenkoa <miranel@gmail.com>
2021-03-09 03:33:12 +0300

    Технический коммит откатила

--- app/src/main/java/ru/lenkoa/android1/calc/SettingsActivity.java
+++ app/src/main/java/ru/lenkoa/android1/calc/SettingsActivity.java
@@ -3,7 +3,6 @@ package ru.lenkoa.android1.calc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -11,16 +10,5 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
-        initView();
-    }
-
-    private void initView() {
-        Button button_theme_first = findViewById(R.id.button_theme_first);
-        button_theme_first.setOnClickListener(); //TODO set theme
-        Button button_theme_second = findViewById(R.id.button_theme_second);
-        button_theme_second.setOnClickListener(); //TODO set theme
-        Button button_back = findViewById(R.id.button_back);
-        button_back.setOnClickListener(); //TODO set theme
     }
 }
